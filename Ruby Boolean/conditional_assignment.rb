1aa351d13332b4d643d864dedfb1e57084eb5a1f
Nicolas Queen <nicolasqueen@gmail.com>
2015-06-28 19:55:08 +0000

    First commit of my TreeHouse Ruby files

---
+++ Ruby Boolean/conditional_assignment.rb
@@ -0,0 +1,19 @@
+# name = "Nick"
+
+# if defined?(name)
+#  name
+# else
+#  name = "Andrew"
+# end
+
+# puts name
+
+# exit
+
+# new_name = "Nick"
+# new_name ||= "Andrew"
+# puts new_name
+
+other_name = "Nick"
+other_name = (other_name || "Tom Sawyer")
+puts other_name
